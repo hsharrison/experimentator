a45374c919785821ebdfd11e9063311ab682d525
Henry Harrison <henry.schafer.harrison@gmail.com>
2013-10-21 01:12:03 -0400

    Better variable names.
    In example, more descriptive variable names.

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ class MyExperiment(exp.Experiment):
     Example 'experiment' with two independent variables, one constant variable, and one random variable.
     """
     def run_trial(self, current_trial, **kwargs):
-        return kwargs['a'] % kwargs['b'] + kwargs['c'] + kwargs['d']
+        return kwargs['iv1'] % kwargs['iv2'] + kwargs['cv'] + kwargs['rv']
 
     def session_start(self):
         print('Starting experiment...')
@@ -22,7 +22,7 @@ class MyExperiment(exp.Experiment):
 
 if __name__ == '__main__':
     # Create the experiment with 4 associated variables.
-    experiment = MyExperiment(a=[4, 10], b=[0, 1, 2], c=3, d=np.random.random)
+    experiment = MyExperiment(iv1=[4, 10], iv2=[0, 1, 2], cv=3, rv=np.random.random)
     # Cross the two independent variables.
     experiment.cross(['a', 'b'])
     # Randomize 8 trials of each type (total here: 8*6 = 48)
