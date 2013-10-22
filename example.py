993efc11366e20f942afce457ffb3818bf96c036
Henry Harrison <henry.schafer.harrison@gmail.com>
2013-10-22 01:56:32 -0400

    Implemented block_list and run_session.

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ class MyExperiment(exp.Experiment):
     """
     Example 'experiment' with two independent variables, one constant variable, and one random variable.
     """
-    def run_trial(self, current_trial, **kwargs):
+    def run_trial(self, trial_idx, **kwargs):
         return kwargs['iv1'] % kwargs['iv2'] + kwargs['cv'] + kwargs['rv']
 
     def session_start(self):
@@ -16,16 +16,12 @@ class MyExperiment(exp.Experiment):
     def session_end(self):
         print('Experiment complete.')
 
-    def inter_trial(self, current_trial, **kwargs):
+    def inter_trial(self, trial_idx, **kwargs):
         print('Moving to trial {}'.format(current_trial))
 
 
 if __name__ == '__main__':
     # Create the experiment with 4 associated variables.
     experiment = MyExperiment(iv1=[4, 10], iv2=[0, 1, 2], cv=3, rv=np.random.random)
-    # Cross the two independent variables.
-    experiment.cross(['a', 'b'])
-    # Randomize 8 trials of each type (total here: 8*6 = 48)
-    experiment.randomize(8)
-    # Run all 48 trials.
-    experiment.run_session()
+    # Randomize 8 trials of each type (total here: 8*6 = 48) and run all 48 trials.
+    experiment.run_session(trials_per_type_per_block=8)
--- experimentator.py
+++ experimentator.py
@@ -1,5 +1,22 @@
 import numpy as np
 import pandas as pd
+import itertools
+import functools
+import warnings
+
+
+def sort_factory(array, n, method):
+    if method == 'random':
+        return functools.partial(np.random.permutation, n * array)
+    #TODO: More sorts (e.g. counterbalance)
+    elif len(method) == len(array):
+        try:
+            return lambda: n * array[method]
+        except ValueError:
+            warnings.warn('Unrecognized sort method {}.'.format(method), stacklevel=2)
+            return sort_factory(array, n, None)
+    else:
+        return lambda: n * array
 
 
 class Variable():
@@ -23,11 +40,20 @@ class ConstantVariable(Variable):
 
 
 class IndependentVariable(Variable):
-    def __init__(self, name, levels, design='within'):
+    """
+    Additional args: levels (list of values for the IV)
+    Additional kwargs: design (between or within), change_by (trial, block, session, participant)
+        design='between' is equivalent to change_by='participant'
+    """
+    def __init__(self, name, levels, design='within', change_by='trial'):
         super(IndependentVariable, self).__init__(name)
         self.levels = levels
         self.n = len(levels)
         self.design = design
+        if self.design == 'between':
+            self.change_by = 'participant'
+        else:
+            self.change_by = change_by
 
     def value(self, idx, *args, **kwargs):
         return self.levels(idx)
@@ -60,21 +86,72 @@ class Experiment():
     """
     Experiments should subclass this and override, at minimum, the method run_trial(settings, current_trial).
     """
+    # TODO: between-subjects design
+    # TODO: multi-session experiments
     def __init__(self, *args, **kwargs):
         self.variables = list(args)
         for k, v in kwargs.items():
             self.variables.append(new_variable(k, v))
 
-    def cross(self, variables):
+        self.trial_ivs = [v for v in self.variables if isinstance(v, IndependentVariable) and v.change_by == 'trial']
+        self.block_ivs = [v for v in self.variables if isinstance(v, IndependentVariable) and v.change_by == 'block']
+
+        self.blocks = []
+        self.n_blocks = 0
+        self.n_trials = 0
+
+    def block_list(self, trials_per_type_per_block, blocks_per_block_type=1, trial_sort='random', block_sort='random'):
+        if self.trial_ivs:
+            iv_idxs = itertools.product(*[v.levels for v in self.trial_ivs])
+            trial_types = [{iv.name: iv.levels[condition[idx]] for idx, iv in enumerate(self.trial_ivs)}
+                           for condition in iv_idxs]
+        else:
+            trial_types = [{v.name: v.value for v in self.variables if v not in self.block_ivs}]
+
+        if self.block_ivs:
+            iv_idxs = itertools.product(*[v.levels for v in self.block_ivs])
+            block_types = [{iv.name: iv.levels[condition[idx]] for idx, iv in enumerate(self.block_ivs)}
+                           for condition in iv_idxs]
+        else:
+            block_types = [{}]
+
+        sort_block = sort_factory(np.array(trial_types), trials_per_type_per_block, trial_sort)
+        sort_session = sort_factory(np.array(block_types), blocks_per_block_type, block_sort)
+
+        blocks = list(sort_session())
+        self.n_blocks = len(blocks)
+        for block in blocks:
+            trials = list(sort_block())
+            self.n_trials += len(trials)
+            [trial.update({k: v for k, v in block.items()}) for trial in trials]
+            yield pd.DataFrame(trials)
+
+    def run_session(self, **kwargs):
+        self.blocks = self.block_list(**kwargs)
+        # TODO: initialize data
+        self.session_start()
+        for block_idx, block in enumerate(self.blocks):
+            self.block_start(block_idx, block)
+            for trial_idx, trial in block.iterrows():
+                self.run_trial(trial_idx, **dict(trial))
+                if trial_idx < len(block) - 1:
+                    self.inter_trial(trial_idx, **dict(trial))
+            self.block_end(block_idx, block)
+            if block_idx < self.n_blocks - 1:
+                self.inter_block(block_idx, block)
+        self.session_end()
+        #TODO: save data
+
+    def run_trial(self, trial_idx, **kwargs):
         pass
 
-    def randomize(self, trials_per_type):
+    def block_start(self, block_idx, block):
         pass
 
-    def run_session(self):
+    def block_end(self, block_idx, block):
         pass
 
-    def run_trial(self, current_trial, **kwargs):
+    def inter_block(self, block_idx, block):
         pass
 
     def session_start(self):
