3a1293c9a3026a74b03f61b881d5d9dead9dd642
Henry Harrison <henry.schafer.harrison@gmail.com>
2013-11-13 00:38:25 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2013 Henry S. Harrison
+
+from distutils.core import setup
+
+setup(name='experimentator',
+      version='0.1',
+      author='Henry S. Harrison',
+      author_email='henry.schafer.harrison@gmail.com',
+      url='https://bitbucket.org/hharrison/experimentator',
+      download_url='https://bitbucket.org/hharrison/experimentator/get/default.tar.gz',
+      description='Experiment Builder',
+      long_description='Turn code for a single trial into an entire experiment by systematically varying parameters',
+      )
