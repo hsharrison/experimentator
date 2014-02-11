599e65e55387f8e01430e46ab4ebedd5f8b4e55d
Henry Harrison <henry.schafer.harrison@gmail.com>
2014-02-10 22:28:48 -0500

    Hyphenate command-line arguments

--- experimentator/__main__.py
+++ experimentator/__main__.py
@@ -1,18 +1,18 @@
 """Usage:
-  experimentator run <experiment_file> (--next <level>  [--not-finished] | (<level> <n>)...) [--demo] [--debug]
-  experimentator  export <experiment_file> <data_file> [--debug]
+  experimentator run <experiment-file> (--next <level>  [--not-finished] | (<level> <n>)...) [--demo] [--debug]
+  experimentator  export <experiment-file> <data-file> [--debug]
   experimentator -h | --help
   experimentator --version
 
 
 Commands:
-  run <experiment_file> --next <level>      Runs the first <level> that hasn't started. E.g.:
+  run <experiment-file> --next <level>      Runs the first <level> that hasn't started. E.g.:
                                               experimentator.py run experiment1.dat --next session
 
-  run <experiment_file> (<level> <n>)...    Runs the section specified by any number of level=n pairs. E.g.:
+  run <experiment-file> (<level> <n>)...    Runs the section specified by any number of level=n pairs. E.g.:
                                               experimentator.py run experiment1.dat participant 3 session 1
 
-  export <experiment_file> <data_file>      Export the data in <experiment_file> to csv format as <data_file>.
+  export <experiment-file> <data-file>      Export the data in <experiment_file> to csv format as <data_file>.
                                               Note: This will not produce readable csv files for experiments with
                                                     results in multi-element data structures (e.g., timeseries, dicts).
 
@@ -35,7 +35,7 @@ if options['--debug']:
     logging.basicConfig(level=logging.DEBUG)
 
 if options['run']:
-    cli_exp = load_experiment(options['<experiment_file>'])
+    cli_exp = load_experiment(options['<experiment-file>'])
     cli_demo = options['--demo']
     if options['--next']:
         cli_section = cli_exp.find_first_not_run(options['<level>'][0], by_started=options['--not-finished'])
@@ -44,4 +44,4 @@ if options['run']:
         run_experiment_section(cli_exp, demo=cli_demo, **dict(zip(options['<level>'], options['<n>'])))
 
 elif options['export']:
-    export_experiment_data(options['<experiment_file>'], options['<data_file>'])
+    export_experiment_data(options['<experiment-file>'], options['<data-file>'])
