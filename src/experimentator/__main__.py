ce7ef2cdfe3e30819ae986423afcb6d481642870
Henry Harrison <henry.schafer.harrison@gmail.com>
2014-07-01 18:54:44 -0400

    no = for single-letter options

--- src/experimentator/__main__.py
+++ src/experimentator/__main__.py
@@ -11,7 +11,7 @@ Run/resume options:
   -h, --help        Show full help.
   --version         Print the installed version number of experimentator.
   -d, --debug       Set logging level to DEBUG.
-  -o=<options>      Pass <options> to the experiment and save it as string in Experiment.session_data['options'].
+  -o <options>      Pass <options> to the experiment and save it as string in Experiment.session_data['options'].
   --demo            Don't save data.
   --not-finished    Run the first <level> that hasn't finished (rather than first that hasn't started).
   --skip-parents    Don't enter context of parent levels.
